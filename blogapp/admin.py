9e0e78d264875a922fa48442e36decad6c4818af
shashivendra <yadav.shashivendra94@gmail.com>
2024-02-19 16:09:55 +0530

    To display list and search field

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import Category, Post, Author, Comment
 
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Comment)
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_name', 'category', 'created_at', 'status')
+    search_fields = ('title', 'author_name__author_name', 'category__name', 'content')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('author_name',)
+    search_fields = ('author_name',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'message')
+    search_fields = ('name', 'email', 'post__title', 'message')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Comment, CommentAdmin)
