ecd28b42c6362c6b481718af068a9f657a81ab16
shashivendra <yadav.shashivendra94@gmail.com>
2024-02-19 16:16:21 +0530

    no changes

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     
     path('', views.home, name="home"),
     path('savequiry/', views.savequiry, name='add_category'),
-    path('addblog/', views.addblog, name='add_blog'),
+    path('add-blog', views.addblog, name='add_blog'),
     path('post-list/',views.post_list, name ='post_list'),
     path('post-detail/<int:post_detail_id>/', views.post_detail, name ='post_detail'),
     path('author-name', views.author_name, name = 'add_name'),
