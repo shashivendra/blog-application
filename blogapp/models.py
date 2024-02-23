d1cd180e9357e0511e94d2cd62f6010898ac4320
shashivendra <yadav.shashivendra94@gmail.com>
2024-02-23 17:57:16 +0530

    confirm password filed added in models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.db.models import Model
 
-class User(models.Model):
-    pass
-
+class Candidate(models.Model):
+     username = models.CharField(primary_key=True, max_length=30)
+     password = models.CharField(null=False, max_length=25)
+     cnf_password = models.CharField(null=False, max_length=25)
+     name = models.CharField(null=False, max_length=20)
+     
+     def __str__(self):
+         return self.username
+     
 
 class Category(models.Model):
     
-    name = models.CharField(max_length=255, name="name", null ="True")
+    name = models.CharField(max_length=255, name="name", null =True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
     
