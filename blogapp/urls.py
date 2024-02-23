0611f0ec5f9b9dd93a3359800e1f6a66d0d83cf3
shashivendra <yadav.shashivendra94@gmail.com>
2024-02-23 17:58:41 +0530

    signup, login and dashoard urls added

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,13 +5,17 @@ urlpatterns = [
     
     # path('', views.home, name="home"),
     # path('savequiry/', views.savequiry, name='add_category'),
+    path('', views.Welcome, name='welcome'),
+    path('sign-up', views.SignUp_Form, name='sign_up'),
+    path('login', views.login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('logout', views.logoutView, name ='logout_view'),
     path('add-blog', views.addblog, name='add_blog'),
     path('post-list/',views.post_list, name ='post_list'),
     path('post-detail/<int:post_detail_id>/', views.post_detail, name ='post_detail'),
     path('author-name', views.author_name, name = 'add_name'),
     path('comment/' ,views.blog_comment, name ='add_comment'),
-    
-    
+     
     ]
      
 
